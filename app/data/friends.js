5edcf4ab049704894660922359fd58b75fd94bc8
Franklin Segura <franklinsegura@Franklins-MacBook-Air.local>
2017-06-16 13:49:30 -0400

    files

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Joise Sanchez",
+    photo: "http://www.headshotlondon.co.uk/wp-content/uploads/2015/05/headshotlondonblog_examples-of-headshots.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Paul Grant",
+    photo: "https://s-media-cache-ak0.pinimg.com/originals/6f/36/c6/6f36c6a487cacf0f564ea539feec0886.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Andrew Gonzalez",
+    photo: "http://3.bp.blogspot.com/-rwOF_DrqZmM/UaOVzdW_JbI/AAAAAAAAF0Y/Nl3opz3pD3w/s1600/804d9b5d74df98baefe8b7fe1356ee9d.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Daniel Gonzalez",
+    photo: "https://d26oc3sg82pgk3.cloudfront.net/files/media/uploads/zinnia/2016/10/27/6-headshot-photograph-myths-busted-by-marc-cartwright.jpg.644x480_q100.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Christian Aromin",
+    photo: "http://www.mikenakamuraphotography.com/img/s4/v64/p2285964296-3.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Ashley Svendsen",
+    photo: "https://picturecorrect-wpengine.netdna-ssl.com/wp-content/uploads/2013/11/modern-corporate-headshot-technique-3.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array.
+module.exports = friends;
